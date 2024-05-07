a1a33a075d1c174b57acded62f072e46bbd43b11
Lawrence Reynolds <lawrence.m.reynolds@googlemail.com>
2024-05-07 16:02:38 +0100

    Now using MySql testcontainer for local development.

--- src/main/java/com/reynolds/composer/v1/composerfacade/ComposerFacadeApplication.java
+++ src/main/java/com/reynolds/composer/v1/composerfacade/ComposerFacadeApplication.java
@@ -3,14 +3,21 @@ package com.reynolds.composer.v1.composerfacade;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
+/* Disabling MongoDB and SQL as neither are used in the facade. */
 @SpringBootApplication(exclude = {
         MongoAutoConfiguration.class,
-        MongoDataAutoConfiguration.class
+        MongoDataAutoConfiguration.class,
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
 })
 @ComponentScan("com.reynolds.composer.v1")
 public class ComposerFacadeApplication {
